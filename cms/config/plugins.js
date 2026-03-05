b499b55e6db1baadb1379a8b4b4ed00af0bbede3
JW000 <2044478382@qq.com>
2026-03-05 15:01:52 +0800

    oss

--- cms/config/plugins.js
+++ cms/config/plugins.js
@@ -9,14 +9,21 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
-      provider: '@strapi/provider-upload-ali-oss',
+      provider: 'strapi-provider-upload-oss',
       providerOptions: {
         accessKeyId: env('OSS_ACCESS_KEY_ID'),
         accessKeySecret: env('OSS_ACCESS_KEY_SECRET'),
         region: env('OSS_REGION'),
         bucket: env('OSS_BUCKET'),
-        endpoint: env('OSS_ENDPOINT'),
-        cname: env.bool('OSS_CNAME', false),
+        uploadPath: env('OSS_UPLOAD_PATH'),
+        baseUrl: env('OSS_BASE_URL'),
+        timeout: env.int('OSS_TIMEOUT', 60),
+        secure: env.bool('OSS_SECURE', true),
+        internal: env.bool('OSS_INTERNAL', false),
+        bucketParams: {
+          ACL: env('OSS_BUCKET_ACL', 'private'),
+          signedUrlExpires: env.int('OSS_SIGNED_URL_EXPIRES', 60 * 60),
+        },
       },
       actionOptions: {
         upload: {},
